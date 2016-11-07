253d931ec960150401aa509a0ee9ac3677884cfa
Marcel Poblet <Mopobl0659@ung.edu>
2016-11-07 07:39:34 -0500

    Add files via upload

--- src/Controller/loginController.java
+++ src/Controller/loginController.java
@@ -1,8 +1,7 @@
 package Controller;
 
-import java.sql.ResultSet;
-
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,15 +10,27 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 
 public class loginController {
-	@FXML private TextField txtUser;
+	@FXML private TextField txtUsername;
 	@FXML private PasswordField txtPassword;
-	@FXML private Button btnLogin;
+	//@FXML private Button btnLogin;
 	@FXML private Label lblStatus, lblActionStatus;
 
-	public void clickOnLogin(ActionEvent event) throws Exception {
-		//requestUserEntry();
-		lblActionStatus.setTextFill(Color.FIREBRICK);
-		lblActionStatus.setText("Sign in Button Pressed!");
+	@FXML
+	public void clickOnLogin(ActionEvent event) 
+	{
+		if(txtUsername.getText().equals("user") && txtPassword.getText().equals("pass"))
+		{
+			lblActionStatus.setText("Success!");
+			lblActionStatus.setTextFill(Color.GREEN);
+		}
+		else
+		{
+			lblActionStatus.setText("Invalid Username or Password!");	
+			lblActionStatus.setTextFill(Color.FIREBRICK);
+		}
+		txtUsername.setText("");
+		txtPassword.setText("");
+	
 	}
 
 	/*private void requestUserEntry() {
